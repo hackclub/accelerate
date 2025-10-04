8f6834f39c0765a055b446442e278774d627b8c4
vandorena <alexvd@hackclub.com>
2025-10-04 09:17:28 -0400

    background

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="relative w-full min-h-screen">
+    <div class="absolute inset-0 z-0">
+        <img src="/top-whiteboard.png" alt="" class="w-full h-auto block" />
+        <img src="/middle-whiteboard.png" alt="" class="w-full h-auto block" />
+        <img src="/middle-2-whiteboard.png" alt="" class="w-full h-auto block" />
+        <img src="/bottom-whiteboard.png" alt="" class="w-full h-auto block" />
+    </div>
+    
+    <div class="relative z-10 p-10">
+        <img src="cloud-note.png" alt="Cloud Note" class="absolute top-30 left-30 -rotate-6 scale-140" />
+    </div>
+</div>
